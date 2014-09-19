ae397ac09b0630246008c3b8a8c870c3165c9380
Angel Hernandez <ah.sec7@gmail.com>
2014-09-18 21:35:02 -0500

    Completed copyspecial.py For Exercise in: https://developers.google.com/edu/python/exercises/copy-special

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -16,7 +16,7 @@ import commands
 """
 
 def get_special_paths(dir):
-  # This function lists all the files in the current directory and
+  # This function lists all the files in a directory and
   # prints the path of the files named following the pattern _w_
   # where w is one or more word chars.
   special_files = []
@@ -29,8 +29,31 @@ def get_special_paths(dir):
   for name in special_files:
     paths.append(os.path.abspath(os.path.join(dir, name)))
   return paths
-  exit(0)
 
+def copy_to(paths, dir):
+  # copy_to(paths, dir) given a list of paths, copies those files into the given directory
+  shutil.copy(paths, dir)
+
+def zip_to(zippath, paths):
+  # zip_to(paths, zippath) given a list of paths, zip those files up into the given zipfile
+  # For a Linux system 
+  paths2 = get_special_paths(paths)
+  to_send = "zip " + zippath[0]
+  for path in paths2:
+    to_send = to_send + " " + path
+  print "Command I will run: " + to_send
+  (status, output) = commands.getstatusoutput(to_send)
+  if status: 
+    print "Output: " + str(output)
+    exit(-1)
+
+def to_dir(dir_dest, dir_orig):
+  # If the "--todir dir" option is present at the start of the command line, do not print 
+  # anything and instead copy the files to the given directory, creating it if necessary. 
+  # Use the python module "shutil" for file copying.
+  paths = get_special_paths(dir_orig)
+  for filename in paths:
+    copy_to(filename, dir_dest)
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -48,23 +71,16 @@ def main():
   # The args array is left just containing the dirs.
   todir = ''
   if args[0] == '--todir':
-    todir = args[1]
+    to_dir(args[1], args[2])
     del args[0:2]
 
   tozip = ''
   if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
+    zip_to(args[1:-1], args[-1])
 
   if len(args) == 0:
     print "error: must specify one or more dirs"
     sys.exit(1)
 
-  # +++your code here+++
-  # Call your functions
-
-  else:
-    get_special_paths(args[0])
-  
 if __name__ == "__main__":
   main()
